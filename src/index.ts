bc5d5f2f31c01207ffdcbc742fae183389c43f5c
Lucas Murta <lucas111murta@gmail.com>
2026-07-06 16:22:16 -0300

    feat(badge): add new component

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { default as StBadge } from './components/badge';
 export { default as StPaper } from './components/paper';
 export { default as StTypography } from './components/typography';
 export type { SizeValue } from './types';
